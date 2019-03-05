4b081b52f90cb054452957bca919e6b7b636ddf3
cwinters8 <clwinters8@gmail.com>
2019-03-04 19:34:11 -0800

    add validation for title and author

--- models/book.js
+++ models/book.js
@@ -14,8 +14,18 @@ const BookModel = (sequelize, DataTypes) => {
       primaryKey: true,
       autoIncrement: true
     },
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {msg: 'Title is required'}
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {msg: 'Author is required'}
+      }
+    },
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER
   });
