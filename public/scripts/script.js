a3cc0527a6bf193e5e2d36441d2a7ef412246308
cwinters8 <clwinters8@gmail.com>
2019-03-10 22:11:03 -0700

    pagination implemented

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,68 @@
-// dynamic search
+const tableBody = document.querySelector('.table tbody');
+const tableRows = document.querySelectorAll('.table tbody tr');
+
+/**
+ * PAGINATION
+ */
+// create an array of arrays, with 10 elements each
+function buildArray(books) {
+  const parentArray = [];
+  books.forEach((book, index) => {
+    const parentLength = parentArray.length;
+    let currentArrayIndex = (parentLength > 0) ? parentLength - 1 : 0;
+    if (index % 10 === 0 && index != 0) {
+      currentArrayIndex++;
+    }
+    if (!parentArray[currentArrayIndex]) {
+      parentArray[currentArrayIndex] = [];
+    }
+    parentArray[currentArrayIndex].push(book);
+  });
+  return parentArray;
+}
+
+// show a given list of books
+function showPage(books) {
+  tableRows.forEach(book => {
+    // unhide all books
+    book.style.display = '';
+    // hide all books except if it's in the given list
+    if (!books.includes(book)) {
+      book.style.display = 'none';
+    }
+  });
+}
+
+// main function to add pagination
+function paginate(arrayOfBooks) {
+  // append page links
+  arrayOfBooks.forEach((array, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `<a class="page-link" href="#">${index + 1}</a>`;
+    li.className = 'page-item';
+    document.querySelector('.pagination').append(li);
+    // add a listener to each item
+    li.addEventListener('click', e => {
+      e.preventDefault();
+      // remove active class from other links
+      document.querySelectorAll('.pagination li').forEach(item => {
+        item.className = 'page-item';
+      })
+      // add active class to this link
+      li.classList += ' active';
+      // show current array
+      showPage(array);
+    });
+  });
+  // select first page by default
+  document.querySelector('.page-item').click();
+}
+
+paginate(buildArray(tableRows));
+
+/**
+ * DYNAMIC SEARCH
+ */
 const searchBox = document.getElementById('search');
 searchBox.addEventListener('keyup', e => {
   const searchTerm = e.target.value.toLowerCase();
