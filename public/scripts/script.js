be47b96ff063637e3a041fa36b907953ed504b58
cwinters8 <clwinters8@gmail.com>
2019-03-11 23:24:02 -0700

    move sort to the top

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,29 @@
 const tableRows = document.querySelectorAll('.table tbody tr');
 
 /**
+ * SORT
+ */
+// event listeners for each sort button
+document.querySelectorAll('a.sort').forEach(sortLink => {
+  sortLink.addEventListener('click', e => {
+    e.preventDefault();
+    const classList = sortLink.classList;
+    const fieldClass = classList[1];
+    const fieldName = fieldClass.match(/^sort-(\w+)$/)[1];
+    // if the class list already has asc, then sort desc, and vice versa
+    if (classList.contains('asc')) {
+      tinysort('tbody tr', {selector: `td.${fieldName}`, order: 'desc'});
+      classList.remove('asc', 'desc');
+      classList.add('desc');
+    } else {
+      tinysort('tbody tr', {selector: `td.${fieldName}`});
+      classList.remove('asc', 'desc');
+      classList.add('asc');
+    }
+  });
+});
+
+/**
  * PAGINATION
  */
 // create an array of arrays, with 10 elements each
@@ -119,61 +142,3 @@ searchBox.addEventListener('keyup', e => {
     hideAll();
   }
 });
-
-/**
- * SORT
- */
-// get the right field from the row
-function getField(fieldName, parentRow) {
-  const fields = parentRow.children;
-  for (let i = 0; i < fields.length; i++) {
-    if (fields[i].className === fieldName) {
-      return fields[i];
-    }
-  }
-}
-
-// sort based on class name match
-function sort(className, order) {
-  const fieldName = className.match(/^sort-(\w+)$/)[1];
-  const booksArray = Array.from(tableRows);
-  // sort by the given field name
-  const sorted = booksArray.sort((a, b) => {
-    const fieldA = getField(fieldName, a);
-    const fieldB = getField(fieldName, b);
-    if (order === 'asc') {
-      return fieldA.innerText < fieldB.innerText ? -1 : 1;
-    } else {
-      return fieldA.innerText > fieldB.innerText ? -1 : 1;
-    }
-  });
-  const tbody = document.querySelector('tbody');
-  // TO-DO: This whole section needs to be fixed. Only currently displayed values should be sorted.
-  // empty tbody
-  tbody.innerHTML = '';
-  // replace with sorted rows
-  sorted.forEach(row => {
-    tbody.append(row);
-  });
-  paginate(sorted);
-}
-
-// event listeners for each sort button
-document.querySelectorAll('a.sort').forEach(sortLink => {
-  sortLink.addEventListener('click', e => {
-    e.preventDefault();
-    const classList = sortLink.classList;
-    const fieldClass = classList[1];
-    const fieldName = fieldClass.match(/^sort-(\w+)$/)[1];
-    // if the class list already has asc, then sort desc, and vice versa
-    if (classList.contains('asc')) {
-      tinysort('tbody tr', {selector: `td.${fieldName}`, order: 'desc'});
-      classList.remove('asc', 'desc');
-      classList.add('desc');
-    } else {
-      tinysort('tbody tr', {selector: `td.${fieldName}`});
-      classList.remove('asc', 'desc');
-      classList.add('asc');
-    }
-  });
-})
