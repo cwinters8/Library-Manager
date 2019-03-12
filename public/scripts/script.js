59b4a0c9a9d3b8798c83f3cdc26383af6133c25b
cwinters8 <clwinters8@gmail.com>
2019-03-11 20:39:09 -0700

    implement sort functionality

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -84,3 +84,59 @@ searchBox.addEventListener('keyup', e => {
   });
   paginate(matched);
 });
+
+/**
+ * SORT
+ */
+// get the right field from the row
+function getField(fieldName, parentRow) {
+  const fields = parentRow.children;
+  for (let i = 0; i < fields.length; i++) {
+    if (fields[i].className === fieldName) {
+      return fields[i];
+    }
+  }
+}
+
+// sort based on class name match
+function sort(className, order) {
+  const fieldName = className.match(/^sort-(\w+)$/)[1];
+  const booksArray = Array.from(tableRows);
+  // sort by the given field name
+  const sorted = booksArray.sort((a, b) => {
+    const fieldA = getField(fieldName, a);
+    const fieldB = getField(fieldName, b);
+    if (order === 'asc') {
+      return fieldA.innerText < fieldB.innerText ? -1 : 1;
+    } else {
+      return fieldA.innerText > fieldB.innerText ? -1 : 1;
+    }
+  });
+  const tbody = document.querySelector('tbody');
+  // empty tbody
+  tbody.innerHTML = '';
+  // replace with sorted rows
+  sorted.forEach(row => {
+    tbody.append(row);
+  });
+  paginate(sorted);
+}
+
+// event listeners for each sort button
+document.querySelectorAll('a.sort').forEach(sortLink => {
+  sortLink.addEventListener('click', e => {
+    e.preventDefault();
+    const classList = sortLink.classList;
+    const fieldClass = classList[1];
+    // if the class list already has asc, then sort desc, and vice versa
+    if (classList.contains('asc')) {
+      sort(fieldClass, 'desc');
+      classList.remove('asc', 'desc');
+      classList.add('desc');
+    } else {
+      sort(fieldClass, 'asc');
+      classList.remove('asc', 'desc');
+      classList.add('asc');
+    }
+  });
+})
